807f0cea094a30636a3b191985f33f309b700f3d
mdenton8 <matthewdenton@comcast.net>
2014-03-11 04:54:25 -0700

    Update to scrape.py
    Works with new SCPD page, asks for the URL instead of the course name, works with new two-factor authentication, doesn't store password in plain-text, does ten at once, and doesn't bother with encoding (just downloads the webm version, it's a nice small version of the video. Download VLC if you can't view them).
    
    Liable to become outdated quickly, as everything is hardcoded in.

--- scrape.py
+++ scrape.py
@@ -1,56 +1,75 @@
+#!/usr/bin/env python
+
 import re
 import os
-import passwords
-import sys
+import getpass
+import errno
+import itertools
 from subprocess import Popen
 from mechanize import Browser
 from BeautifulSoup import BeautifulSoup
 from multiprocessing import Pool
 
-def download(work):
-	if os.path.exists(work[1]):
-		print "Already downloaded", work
-		return
 
-	print "Starting", work
-	# Put it in a temp file
-	if os.system("mimms -c %s %s" % (work[0], "_" + work[1])) == 0:
-		# Move the file on success
-		os.system("mv %s %s" % ("_" + work[1], work[1]))
-	print "Finished", work
+def make_sure_path_exists(path):
+        print path
+        try:
+                os.makedirs(path)
+        except OSError as exception:
+                if exception.errno != errno.EEXIST:
+                        raise
+
+def download(url, direc):
+	"""Copy the contents of a file from a given URL
+	to a local file.
+	"""
+        make_sure_path_exists(os.getcwd()+ os.sep + direc + os.sep);
+	
+        if os.path.exists(os.getcwd()+ os.sep + direc + os.sep + url.split('/')[-1]):
+		print "Already downloaded", url
+		return
+	
+	import urllib
+	webFile = urllib.urlopen(url)
+	localFile = open(os.getcwd()+ os.sep + direc + os.sep + url.split('/')[-1], 'wb')
+	localFile.write(webFile.read())
+	webFile.close()
+	localFile.close()
+
+def download_star(url_direc):
+        download(*url_direc)
 
 if __name__ == '__main__':
 	# Pretend we're just a regular old user (this is naughty, don't try this at home kids)
 	br = Browser()
 	br.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6; en-us) AppleWebKit/531.9 (KHTML, like Gecko) Version/4.0.3 Safari/531.9')]
 	br.set_handle_robots(False)
-	br.open("https://myvideosu.stanford.edu/oce/currentquarter.aspx")
+	br.open(raw_input('Enter course page URL: '))
 	assert br.viewing_html()
 
 	# Import from a module outside of version control your SUNET id and password
 	br.select_form(name="login")
-	br["username"] = passwords.my_username
-	br["password"] = passwords.my_password
+	br["username"] = raw_input('Username: ')
+	br["password"] = getpass.getpass();
 
 	# Open the course page for the title you're looking for 
-	response = br.submit()
-	response = br.follow_link(text=sys.argv[1])
+        br.submit()
+        br.select_form(name="login")
+        br["otp"] = raw_input('Enter Two-Factor Authentification: ')
+        response = br.submit()
+        
+        direc = raw_input("Directory to store videos: ")
 
 	# Build up a list of lectures
 	links = []
-	for link in br.links(text="WMP"):
-		links.append(re.search(r"'(.*)'",link.url).group(1))
+	for link in br.links(text="Watch Now"):
+                link2 = link.attrs[7]
+                link3 = eval(link2[1])
+                print link3['url'][1]['webm-url']
+		links.append(link3['url'][1]['webm-url'])
 
-	videos = []
-	# These are done serially purely just to not look suspicious, we could probably parallelize this as well
-	for link in links:
-		response = br.open(link)
-		soup = BeautifulSoup(response.read())
-		video = soup.find('object', id='WMPlayer')['data']
-		video = re.sub("http","mms",video)
-		output_name = re.search(r"[a-z]+[0-9]+[a-z]?/[0-9]+",video).group(0).replace("/","_") + ".wmv"
-		videos.append((video, output_name))
+	print "Downloading %d videos..." % len(links)
 
 	# Make a thread pool and download 5 files at a time
-	p = Pool(processes=5)
-	p.map(download, videos)
+	p = Pool(processes=10)
+	p.map(download_star, itertools.izip(links, itertools.repeat(direc)))
